98aa4a2fd5f11b941205ae6e2eda0bd5a7f19dba
ruhtra <kawanarthurskate@gmail.com>
2025-07-15 21:32:33 -0300

    First commit

---
+++ Assets/Scripts/Core/Controller.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using TMPro;
+
+namespace MyProject.Core
+{
+    public class Controller : MonoBehaviour
+    {
+    	public Data data;
+    	[SerializeField] private TMP_Text flasksText;
+
+    	private void Start()
+    	{
+    		data = new Data();
+    	}
+
+    	private void Update()
+    	{
+    		flasksText.text = data.flasks + " flasks";
+    	}
+
+    	public void GenerateFlasks()
+    	{
+    		data.flasks++;
+    	}
+    } 
+}
---
+++ Assets/Scripts/Core/Data.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+
+namespace MyProject.Core
+{
+    public class Data
+    {
+        public double flasks;
+
+        public Data()
+        {
+            flasks = 0;
+        }
+    }
+
+}
